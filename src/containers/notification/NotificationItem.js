1e87e13e8b3dd45fdac71eedc8ddf6d6581813e9
Airing <gx-deng@163.com>
2018-06-16 14:04:35 +0800

    refactor: 优化通知时间的样式

--- src/containers/notification/NotificationItem.js
+++ src/containers/notification/NotificationItem.js
@@ -36,11 +36,11 @@ export default class NotificationItem extends Component {
     const date = new Date(ts)
     Y = date.getFullYear() + '-'
     M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
-    D = date.getDate() + ' '
-    h = date.getHours() + ':'
-    m = date.getMinutes() + ':'
-    s = date.getSeconds()
-    return Y + M + D + h + m + s
+    D = (date.getDate() < 10 ? '0' + date.getDate(): date.getDate()) + ' '
+    h = (date.getHours() < 10 ? '0' + date.getHours(): date.getHours()) + ':'
+    m = (date.getMinutes() < 10 ? '0' + date.getMinutes(): date.getMinutes())
+    s = ':' + (date.getSeconds() < 10 ? '0' + date.getSeconds(): date.getSeconds())
+    return Y + M + D + h + m
   }
 
   _jumpWeb() {
