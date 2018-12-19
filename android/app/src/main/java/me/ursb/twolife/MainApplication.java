3eb84f7e1b6b7f6fd5c32e06132bbf602eeccbde
Airing <airing@ursb.me>
2018-12-19 19:01:09 +0800

    chore: add react-native-view-shot

--- android/app/src/main/java/me/ursb/twolife/MainApplication.java
+++ android/app/src/main/java/me/ursb/twolife/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 
 import com.facebook.react.ReactApplication;
+import fr.greweb.reactnativeviewshot.RNViewShotPackage;
 import com.rnfingerprint.FingerprintAuthPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -39,6 +40,7 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(new MainReactPackage(),
+            new RNViewShotPackage(),
             new FingerprintAuthPackage(),
             new LinearGradientPackage(),
             //new ExtraDimensionsPackage(),
