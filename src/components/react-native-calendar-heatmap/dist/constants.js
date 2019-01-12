89980ddca496061caea0dc75665374feea0364e2
Airing <airing@ursb.me>
2019-01-12 09:39:35 +0800

    docs: update v2.1.12

---
+++ src/components/react-native-calendar-heatmap/dist/constants.js
@@ -0,0 +1,18 @@
+export const MILLISECONDS_IN_ONE_DAY = 24 * 60 * 60 * 1000
+
+export const DAYS_IN_WEEK = 7
+
+export const MONTH_LABELS = {
+  0: '1月',
+  1: '2月',
+  2: '3月',
+  3: '4月',
+  4: '5月',
+  5: '6月',
+  6: '7月',
+  7: '8月',
+  8: '9月',
+  9: '10月',
+  10: '11月',
+  11: '12月',
+}
